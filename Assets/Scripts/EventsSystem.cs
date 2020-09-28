654843cfdeb969e448a5b17e820a41f64e1611fd
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-09-27 21:45:57 -0500

    Player object created and initial movement controls implemented

---
+++ Assets/Scripts/EventsSystem.cs
@@ -0,0 +1,21 @@
+﻿public static class EventsSystem
+{
+    public delegate void OnPlayerMoved(int _numberOfStepsRemaining);
+    static event OnPlayerMoved onPlayerMoved;
+
+    public static void ADD_PlayerMoveedListener(EventsSystem.OnPlayerMoved _method)
+    {
+        onPlayerMoved += _method;
+    }
+    public static void REMOVE_PlayerMoveedListener(EventsSystem.OnPlayerMoved _method)
+    {
+        onPlayerMoved -= _method;
+    }
+    public static void PlayerMoved(int _numberOfStepsRemaining)
+    {
+        if (onPlayerMoved != null)
+        {
+            onPlayerMoved(_numberOfStepsRemaining);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left);
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right);
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up);
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down);
+        }
+    }
+
+}
