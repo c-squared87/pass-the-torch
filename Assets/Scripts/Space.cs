6feba35a66e31cb1dea33d2b38d726f29951f05e
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-09-27 21:34:07 -0500

    basic Space class built

---
+++ Assets/Scripts/Space.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum SPACE_STATE
+{
+    OPEN,
+    BLOCKED,
+    START_SPACE,
+    EXIT_SPACE
+}
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class Space : MonoBehaviour
+{
+    [SerializeField] GameObject openSprite;
+    [SerializeField] GameObject blockedSprite;
+    [SerializeField] GameObject startSprite;
+    [SerializeField] GameObject exitSpace;
+
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField] SPACE_STATE currentSPACE_STATE = SPACE_STATE.OPEN;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        SetSprite();
+    }
+
+    void SetSprite()
+    {
+        switch (currentSPACE_STATE)
+        {
+            case SPACE_STATE.OPEN:
+                spriteRenderer.color = Color.grey;
+                break;
+            case SPACE_STATE.BLOCKED:
+                spriteRenderer.color = Color.red;
+                break;
+            case SPACE_STATE.START_SPACE:
+                spriteRenderer.color = Color.green;
+                break;
+            case SPACE_STATE.EXIT_SPACE:
+                spriteRenderer.color = Color.yellow;
+                break;
+        }
+    }
+}
