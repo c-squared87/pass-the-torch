3a0c41714b0d0e7e8857bc06e783b079a46b51a1
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-09-28 17:23:43 -0500

    feature: added CameraController to follow player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Transform target;
+
+    [SerializeField] float xoffset, yoffset, moveSpeed;
+
+    void LateUpdate()
+    {
+        if (target == null) { target = FindObjectOfType<Player>().transform; }
+
+        Vector3 _position = new Vector3(target.position.x + xoffset, target.position.y + yoffset, -10);
+
+        transform.position = Vector3.MoveTowards(transform.position, _position, Time.deltaTime * moveSpeed);
+    }
+}
