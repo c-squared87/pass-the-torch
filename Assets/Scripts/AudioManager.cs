1a6f1f9d98f31509f05966d0e622c77a6654a06d
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-09-28 17:52:49 -0500

    feature: added audio manager and sound class

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public Sound[] Sounds;
+
+
+    private void Awake()
+    {
+        if (Instance == null) { Instance = this; }
+        if (Instance != this) { Destroy(gameObject); }
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound sound in Sounds)
+        {
+            sound.Source = gameObject.AddComponent<AudioSource>();
+
+            sound.Source.clip = sound.Clip;
+            sound.Source.volume = sound.Volume;
+            sound.Source.pitch = sound.Pitch;
+            sound.Source.loop = sound.Loop;
+
+        }
+
+        Play("BackgroundMusic");
+    }
+
+    public void Play(string _soundToPlay)
+    {
+        Sound _s = Array.Find(Sounds, sound => sound.Name == _soundToPlay);
+
+        if (_s == null) { Debug.Log("SOUND " + _soundToPlay + " NOT FOUND"); return; }
+
+        _s.Source.Play();
+        Debug.Log("playing...");
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    [HideInInspector] public AudioSource Source;
+
+    public string Name;
+    public AudioClip Clip;
+
+    [Range(0,1)] public float Volume;
+    [Range(0.1f,3)] public float Pitch;
+    [Range(0,1.1f)] public float Reverb;
+
+    public bool Loop;
+}
