10e05bdb2594ce69e881504507d75ec07ce16c57
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-09-30 17:00:23 -0500

    feature: moved tutorial to own scene and building it out

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,127 +1,63 @@
 ﻿using UnityEngine.UI;
-using System.Collections.Generic;
+// using System.Collections.Generic;
 using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
-    [SerializeField] Text text;
+    public Text text_1, text_2;
 
-    Player player;
+    [SerializeField] GameObject newGameButton, mainMenuButton, clickthroughButton;
 
-    public string[] Prompts = new string[]{
-        "say hello to torch. move torch with wasd or arrow keys.",
-        "torch has fuel, see the number? walk to the exit.",
-        "not going to make the exit? go to a fuel point to refill."
-        // GOOD LUCK!
-        // - START GAME
-        // - MAIN MENU
+    [SerializeField] GameObject playerItem, exitItem, torchItem;
+
+    public string[] Prompts1 = new string[]{
+        "This is Torch. Say Hello!",
+        "Get Torch to the exit!",
+        "Need more steps?",
+        "Ready to play?"
+    };
+    public string[] Prompts2 = new string[]{
+        "Move Torch with the Arrow keys",
+        "moving torch costs steps",
+        "snag some at a fuel stop!",
+        "    "
     };
 
-    public Transform[] StartPoints;
+    int currentTutorialStep;
 
-    int currentTutorialStep = 0;
-
-    public void AdvanceTutorial()
+    private void Start()
     {
-        player = FindObjectOfType<Player>();
+        if (text_1 == null) { text_1 = GetComponent<Text>(); }
+        if (text_2 == null) { text_2 = GetComponent<Text>(); }
 
+        InitTutorial();
+    }
+
+    void InitTutorial()
+    {
+        currentTutorialStep = 0;
+
+        playerItem.SetActive(false);
+        // exitItem.SetActive(false);
+        // torchItem.SetActive(false);
+
+        newGameButton.SetActive(false);
+        mainMenuButton.SetActive(false);
+
+        playerItem.SetActive(true);
+
+        UpdateUI();
+    }
+
+    public void AdvanceTutorial() // THIS IS CALLED FROM A SCREEN COVERING UI BUTTON
+    {
         currentTutorialStep++;
-        transform.position = StartPoints[currentTutorialStep].position;
-        player.transform.position = transform.position;
         UpdateUI();
     }
 
     void UpdateUI()
     {
-        if (text == null) { text = GetComponent<Text>(); }
-        text.text = Prompts[currentTutorialStep];
+        text_1.text = Prompts1[currentTutorialStep];
+        text_2.text = Prompts2[currentTutorialStep];
     }
-
-    #region REPURPOSED LEVELMANAGER CODE BELOW
-
-    public Dictionary<Vector3, Space> Spaces;
-    public Dictionary<Vector3, TorchPoint> TorchPoints;
-
-    // public Vector3 LevelStartLocation { get; private set; }
-
-    GameObject[] activeOnWin;
-    GameObject[] activeOnLoss;
-
-    private void OnEnable()
-    {
-        InitUI();
-
-        if (TorchPoints == null) { TorchPoints = new Dictionary<Vector3, TorchPoint>(); }
-        if (Spaces == null) { Spaces = new Dictionary<Vector3, Space>(); }
-
-        // EventsSystem.ADD_GameLoseListener(LoseScreen);
-        // EventsSystem.ADD_GameWinListener(WinScreen);
-    }
-
-    private void OnDisable()
-    {
-        // EventsSystem.REMOVE_GameLoseListener(LoseScreen);
-        // EventsSystem.REMOVE_GameWinListener(WinScreen);
-    }
-
-    private void InitUI()
-    {
-        activeOnWin = GameObject.FindGameObjectsWithTag("ActiveOnGameWon");
-        activeOnLoss = GameObject.FindGameObjectsWithTag("ActiveOnGameOver");
-
-        foreach (GameObject gameObject in activeOnWin) { gameObject.SetActive(false); }
-        foreach (GameObject gameObject in activeOnLoss) { gameObject.SetActive(false); }
-    }
-
-    // private void WinScreen()
-    // {
-    //     foreach (GameObject gameObject in activeOnWin) { gameObject.SetActive(true); }
-    //     FindObjectOfType<Player>().enabled = false;
-    // }
-
-    // private void LoseScreen()
-    // {
-    //     foreach (GameObject gameObject in activeOnLoss) { gameObject.SetActive(true); }
-    //     FindObjectOfType<Player>().enabled = false;
-    // }
-
-    public bool CanMove(Vector3 _locationToCheck)
-    {
-        if (TorchPoints.ContainsKey(_locationToCheck)) { TorchPoints[_locationToCheck].HandOffTorch(); }
-
-        if (Spaces[_locationToCheck].currentSPACE_STATE != SPACE_STATE.BLOCKED)
-        {
-            if (Spaces[_locationToCheck].currentSPACE_STATE == SPACE_STATE.EXIT_SPACE) { EventsSystem.GameWon(); }
-            // Spaces[_locationToCheck].currentSPACE_STATE = SPACE_STATE.BLOCKED;
-            return true;
-        }
-        return false;
-    }
-
-    public void UpdateSpace(Space _space)
-    {
-        // if (_space.currentSPACE_STATE == SPACE_STATE.START_SPACE)
-        // {
-        //     LevelStartLocation = _space.SpaceLocation;
-        //     return;
-        // }
-
-        if (Spaces == null) { Spaces = new Dictionary<Vector3, Space>(); }
-
-        Spaces.Add(_space.SpaceLocation, _space);
-    }
-
-    public void UpdateTorchPoints(TorchPoint _point)
-    {
-        if (TorchPoints == null) { TorchPoints = new Dictionary<Vector3, TorchPoint>(); }
-
-        TorchPoints.Add(_point.TorchPointLocation, _point);
-    }
-
-    public void RemoveTorchPoint(TorchPoint _point)
-    {
-        TorchPoints.Remove(_point.TorchPointLocation);
-    }
-    
-    #endregion
 }
